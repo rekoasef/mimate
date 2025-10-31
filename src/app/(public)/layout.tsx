2f3e8718cfb9978a73b2ba2b3a713768c4e4f153
Renzo Asef <renzoasef02@gmail.com>
2025-10-31 08:32:14 -0300

    m

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,22 +3,11 @@ import { ReactNode } from 'react';
 import Header from './_components/Header';
 import Footer from './_components/Footer';
 import WhatsAppButton from './_components/WhatsAppButton';
+import './public-layout.css'; // 👈 nuevo archivo que creamos
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="relative flex flex-col min-h-screen text-white overflow-hidden">
-      {/* Fondo real como imagen */}
-      <div className="absolute inset-0 -z-10">
-        <picture>
-          <source media="(min-width: 768px)" srcSet="/background-desktop-pattern.jpg" />
-          <img
-            src="/background-mobile-pattern.jpg"
-            alt="Fondo textura Mimate"
-            className="w-full h-full object-cover"
-          />
-        </picture>
-      </div>
-
+    <div className="public-layout text-white flex flex-col min-h-screen">
       <header className="sticky top-0 z-50 w-full">
         <Header />
       </header>
